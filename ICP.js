129b007c61356ec1e4c00eea3b9bf8b839912336
brosquinha <brosquinha@users.noreply.github.com>
2018-04-22 19:54:47 -0300

    Testando identificação precode de editor e testes de VE

--- ICP.js
+++ ICP.js
@@ -6,7 +6,7 @@
 
 var SWWICP = (function($) {
 	"use strict";
-	var ICPversion = '2.7.2';
+	var ICPversion = '2.7.3-beta.0';
 	var artigoNome, artigoTitulo;
 	var artigoTexto = '';
 	var artigoTipo = '';
@@ -215,12 +215,10 @@ var SWWICP = (function($) {
 	{
 		$("#CuratedContentToolModal header h3").text("Passo 1: Universo");
 		var passo1, txtBotaoSim, txtBotaoNao;
-		userActions.editor = (window.wgAction == 'edit') ? "source" : "VE";
 		if (window.wgAction == 'edit' && $("#cke_21_label").length == 1)
 		{
 			window.CKEDITOR.tools.callFunction(56); //For WYSIWYG editor
 			ICP_wys = true;
-			userActions.editor = "WYSIWYG";
 		}
 		if (infoboxesForTitle.indexOf(infoboxName) > -1)
 			artigoTexto = "{{Title|"+artigoTitulo+"}}\n";
@@ -613,14 +611,58 @@ var SWWICP = (function($) {
 			$($("div.oo-ui-toolbar-tools div.oo-ui-widget.oo-ui-widget-enabled.oo-ui-toolGroup.oo-ui-iconElement.oo-ui-indicatorElement.oo-ui-popupToolGroup.oo-ui-listToolGroup")[0]).addClass('oo-ui-popupToolGroup-active oo-ui-popupToolGroup-left');
 			$("span.oo-ui-tool-name-wikiaSourceMode").css('border', '1px solid');
 			$("span.oo-ui-tool-name-wikiaSourceMode a").click(function() {
-				setTimeout(function() {
-					if ($("textarea.ui-autocomplete-input").val().search("\\[\\[Categoria:") >= 0)
-						$("textarea.ui-autocomplete-input").val(artigoTexto+"\n\n"+$("textarea.ui-autocomplete-input").val());
-					else
-						$("textarea.ui-autocomplete-input").val(artigoTexto);
-					$("textarea.ui-autocomplete-input").change();
-					setTimeout(function() {$("div.oo-ui-widget.oo-ui-widget-enabled.oo-ui-buttonElement.oo-ui-labelElement.oo-ui-flaggedElement-progressive.oo-ui-flaggedElement-primary.oo-ui-buttonWidget.oo-ui-actionWidget.oo-ui-buttonElement-framed a.oo-ui-buttonElement-button").click();}, 1000);
-				}, 2000);
+				console.log(botaoParaClicar+" clicado");
+				var observarModal = new MutationObserver(function (mutacao, observ) {
+					console.log("Mudei");
+					if ($("div.oo-ui-window-content.oo-ui-dialog-content.oo-ui-processDialog-content.ve-ui-wikiaSourceModeDialog-content").hasClass('oo-ui-window-content-ready'))
+					{
+						console.log("Classe mudada");
+						var textoAInserir = ($("textarea.ui-autocomplete-input").val().search("\\[\\[Categoria:") >= 0) ? artigoTexto+"\n\n"+$("textarea.ui-autocomplete-input").val() : artigoTexto;
+						$("textarea.ui-autocomplete-input").val(textoAInserir);
+						$("textarea.ui-autocomplete-input").change();
+						var intervalo = setInterval(function() {
+						//TODO: Testar tudo isso à exaustão
+							if ($("textarea.ui-autocomplete-input").val() !== textoAInserir)
+							{
+								$("textarea.ui-autocomplete-input").val(textoAInserir);
+								$("textarea.ui-autocomplete-input").change();
+								observ.disconnect()
+								clearInterval(intervalo);
+								setTimeout(function() {
+									$("div.oo-ui-widget.oo-ui-widget-enabled.oo-ui-buttonElement.oo-ui-labelElement.oo-ui-flaggedElement-progressive.oo-ui-flaggedElement-primary.oo-ui-buttonWidget.oo-ui-actionWidget.oo-ui-buttonElement-framed a.oo-ui-buttonElement-button").click();
+										var observarConteudo = new MutationObserver(function(mutacaoJanela, observJanela) {
+										observJanela.disconnect();
+										var observarBody = new MutationObserver(function(mutacaoBody, observBody) {
+											//TODO: testar e terminar confirmação de inserção bem-sucedida do wikitexto
+											console.log($("#title-eraicons").length == 1);
+											userActions.autoInsertSuccess = ($("#title-eraicons").length == 1);
+											if ($("#title-eraicons").length != 1)
+											{
+												$($("div.oo-ui-toolbar-tools div.oo-ui-widget.oo-ui-widget-enabled.oo-ui-toolGroup.oo-ui-iconElement.oo-ui-indicatorElement.oo-ui-popupToolGroup.oo-ui-listToolGroup")[0]).addClass('oo-ui-popupToolGroup-active oo-ui-popupToolGroup-left');
+												$("span.oo-ui-tool-name-wikiaSourceMode").css('border', '1px solid');
+												var backupTextarea = '<p>Infelizmente não foi possível registrar automaticamente seu progresso e, portanto, você deve fazê-lo manualmente.'+
+												' Por favor, copie o código abaixo e volte em '+botaoParaClicar+' e cole-o lá.</p>'+
+												'<textarea id="backupTextarea" style="width:590px; height: 210px">'+textoAInserir+'</textarea>';
+												$.showCustomModal('Ocorreu um problema', backupTextarea, {
+													id: 'ModalBackupWindow',
+													width: 600,
+													height: 350
+												});
+												$("#backupTextarea").on("copy", function() {
+													$("#ModalBackupWindow").closeModal();
+												});
+											}
+											observBody.disconnect();
+										});
+										observarBody.observe(document.body, {childList: true});
+									});
+									observarConteudo.observe($("div.ve-ce-surface.mw-body-content").parent()[0], {childList: true});
+								}, 1000);
+							}
+						}, 250);
+					}
+				});
+				observarModal.observe($("div.oo-ui-window-content.oo-ui-dialog-content.oo-ui-processDialog-content.ve-ui-wikiaSourceModeDialog-content")[0], {attributes: true});
 			});
 		}
 		else
@@ -692,6 +734,10 @@ var SWWICP = (function($) {
 		userActions.whereFrom = document.location.href; //So that I know if they're coming from redlinks, Special:CreatePage or other flows
 		userActions.version = ICPversion;
 		userActions.errors = []
+		userActions.editor = (window.wgAction == 'edit') ? "source" : "VE";
+		if (window.wgAction == 'edit' && $("#cke_21_label").length == 1)
+			userActions.editor = "WYSIWYG";
+		
 		if (window.wgArticleId === 0 && (window.wgNamespaceNumber == 114 || window.wgNamespaceNumber === 0) || (window.wgNamespaceNumber == -1 && window.wgTitle == "CreatePage"))
 		{
 			if (window.wgNamespaceNumber == -1)
