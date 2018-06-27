641f70fc5003c8a67c3e02c409338a59f32f15fc
brosquinha <brosquinha@users.noreply.github.com>
2018-06-26 23:16:37 -0300

    Atualizando versão beta

--- ICP.js
+++ ICP.js
@@ -6,7 +6,7 @@
 
 var SWWICP = (function($) {
 	"use strict";
-	var ICPversion = '2.7.4-beta.0';
+	var ICPversion = '2.7.4-beta.1';
 	var artigoNome, artigoTitulo;
 	var artigoTexto = '';
 	var artigoTipo = '';
